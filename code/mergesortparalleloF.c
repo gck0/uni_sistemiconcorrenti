f6376f8bee39cabd994094f04638d2d21a4229e4
gck0 <20210112+gck0@users.noreply.github.com>
2021-08-21 16:46:34 +0200

    Update mergesortparalleloF.c

--- code/mergesortparalleloF.c
+++ code/mergesortparalleloF.c
@@ -131,7 +131,7 @@ int main(int argc, char **argv)
     }
 
     // VISUALIZZA REPORT
-    printf("id:%d; \tmiss:%d; \ts:%d; \tp:%d processors; \ttime: %lld ms\n",id,countCacheMiss,s,p,(stopT-startT));
+    printf("rank(id):%d \ttcm:%d \ts:%d \tp:%d processors \ttime:%lld ms\n",id,countCacheMiss,s,p,(stopT-startT));
     
     // VISUALIZZA ARRAY ORDINATO       
     /*
