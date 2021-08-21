91c3650d60821fcf4d0954de4714b1fc923f87be
gck0 <20210112+gck0@users.noreply.github.com>
2021-08-21 19:33:59 +0200

    Update mergesortparalleloV2.c

--- code/mergesortparalleloV2.c
+++ code/mergesortparalleloV2.c
@@ -96,7 +96,7 @@ int main(int argc, char** argv) {
 		*/
 		
 		/*****CALCOLO TEMPO******/
-		printf("tempo totale: %lld",PAPI_get_real_usec()-totalTime);
+		printf("tempo totale: %lld us\n",PAPI_get_real_usec()-totalTime);
 			
 		/********** Clean up root **********/
 		free(sorted);
