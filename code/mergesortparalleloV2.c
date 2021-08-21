0255be8c19d76052658bdac5ab87aeb028b90692
gck0 <20210112+gck0@users.noreply.github.com>
2021-08-21 19:26:32 +0200

    Update mergesortparalleloV2.c

--- code/mergesortparalleloV2.c
+++ code/mergesortparalleloV2.c
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
 	int world_rank;
 	int world_size;
 	
-	MPI_INIT(&argc, &argv);
+	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 		
