c344b1f56f2922fcb1cbe2255819d10cb7306134
gck0 <20210112+gck0@users.noreply.github.com>
2021-08-21 17:28:28 +0200

    Update auto.sh

--- code/auto.sh
+++ code/auto.sh
@@ -3,5 +3,5 @@ SUFFIX=$1
 ELEMENTS=$2
 
 git pull
-mpicc $"FILE".c /usr/local/lib/libpapi.a -lm -o mergesortparallelo$"SUFFIX"
-mpirun $"FILE" $"ELEMENTS"
+mpicc $FILE.c /usr/local/lib/libpapi.a -lm -o mergesortparallelo$SUFFIX
+mpirun $FILE $ELEMENTS
